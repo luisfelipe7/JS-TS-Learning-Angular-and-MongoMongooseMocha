6f6f6a4853a9a506ae2d9baf270a3fc7e6562b26
Felipe Castro <luisfelipecs97@gmail.com>
2021-12-20 22:11:38 -0600

    Implemented a new theme and added the CSS and JS

--- Advanced Programs (Using Angular)/Final-Angular-Project/src/app/duration-calculator/duration-calculator.component.ts
+++ Advanced Programs (Using Angular)/Final-Angular-Project/src/app/duration-calculator/duration-calculator.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DurationCalculatorModel } from './duration-calculator.model'
+// Importing Jquery
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-duration-calculator',
   templateUrl: './duration-calculator.component.html',
-  styleUrls: ['./duration-calculator.component.css']
+  styleUrls: ['./duration-calculator.component.css', '../../assets/themeForCounter/css/style.css', '../../assets/themeForCounter/vendor/bootstrap-icons/bootstrap-icons.css']
+
 })
 export class DurationCalculatorComponent implements OnInit {
   public dCModel: DurationCalculatorModel;
@@ -18,7 +21,12 @@ export class DurationCalculatorComponent implements OnInit {
   ngOnInit(): void {
     // Start the updating of the time
     this.startExecution();
+
+    // PENDING TO FIX
+   // this.loadScript("src/assets/themeForCounter/js/script.js");
+
   }
+
 
   // Function in charge of start executing the function in charge of updating the time each second
   startExecution() {
@@ -31,6 +39,16 @@ export class DurationCalculatorComponent implements OnInit {
   // Function that cancel the execution of the function in charge of updating the time each second
   stopExecution() {
     clearInterval(this.myInterval);
+  }
+
+  public loadScript(url: any) {
+    var node = document.createElement('script');
+    node.src = url;
+    console.log('URL '+node.src);
+    node.src = "src/assets/themeForCounter/js/script.js";
+    node.type = 'text/javascript';
+    console.log('URL '+node.src);
+    document.getElementsByTagName('head')[0].appendChild(node);
   }
 
 }
